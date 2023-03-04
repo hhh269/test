695310316cf3d3865715a085321b0cc2b61b263d
前海端游 <2591658566@qq.com>
2023-03-04 22:50:17 +0800

    lh

---
+++ 草稿.sql
@@ -0,0 +1,8 @@
+-- select username,password from users
+-- insert into users (username,password) values ('lm','121212')
+-- select*from users
+-- update users set password='8888888' where id<3
+-- select*from users
+-- delete from users where id=5
+-- select*from users
+select*from users order by id desc
